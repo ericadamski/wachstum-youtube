e3a96493a8805bcf5d9a53d2f79b55712384272f
Eric Adamski <er.adamski@gmail.com>
2020-06-04 13:39:15 -0400

    Prep test for level.codes

---
+++ .level.codes/index.md
@@ -0,0 +1,18 @@
+---
+title: "A test"
+description: "A description"
+points: 600
+---
+
+## Some stuff
+
+another things
+
+```javascript
+// some code
+function () {
+    async () => {
+        return null;
+    }
+}
+```
---
+++ .level.codes/score/login.test.js
@@ -0,0 +1,184 @@
+jest.mock("isomorphic-unfetch", () =>
+  jest.fn(() =>
+    Promise.resolve({
+      ok: true,
+      text() {
+        return null;
+      },
+    })
+  )
+);
+
+import { render, fireEvent, screen, waitFor } from "@testing-library/react";
+import fetch from "isomorphic-unfetch";
+import LoginPage from "../../pages/login";
+
+describe("Login Page", () => {
+  describe("Logging in", () => {
+    it("should allow me to log in", async () => {
+      render(<LoginPage />);
+
+      const email = "email@email.com";
+      const password = "password";
+
+      fireEvent.change(screen.getByLabelText("✉️ Email"), {
+        target: {
+          value: email,
+        },
+      });
+
+      fireEvent.change(screen.getByLabelText("🔑 Password"), {
+        target: {
+          value: password,
+        },
+      });
+
+      fireEvent.click(screen.getByText("Log in"));
+
+      await waitFor(() =>
+        expect(fetch).toHaveBeenCalledWith(
+          "/api/authenticate",
+          expect.objectContaining({
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              name: "",
+              email,
+              password,
+              isSignUp: false,
+            }),
+          })
+        )
+      );
+    });
+
+    it("should handle errors on login", async () => {
+      const error = "There was an error";
+
+      fetch.mockImplementationOnce(() =>
+        Promise.resolve({
+          ok: false,
+          text: () => Promise.resolve(error),
+        })
+      );
+
+      render(<LoginPage />);
+
+      const email = "email@email.com";
+      const password = "password";
+
+      fireEvent.change(screen.getByLabelText("✉️ Email"), {
+        target: {
+          value: email,
+        },
+      });
+
+      fireEvent.change(screen.getByLabelText("🔑 Password"), {
+        target: {
+          value: password,
+        },
+      });
+
+      fireEvent.click(screen.getByText("Log in"));
+
+      await waitFor(() => expect(() => screen.getByText(error)).not.toThrow());
+    });
+  });
+
+  describe("Sign up", () => {
+    it("should allow me to sign up", async () => {
+      render(<LoginPage />);
+
+      const name = "name";
+      const email = "email@email.com";
+      const password = "password";
+
+      fireEvent.click(screen.getByText("Sign up"));
+
+      await waitFor(() => screen.getByLabelText("👩‍💻 Name"));
+
+      fireEvent.change(screen.getByLabelText("👩‍💻 Name"), {
+        target: {
+          value: name,
+        },
+      });
+
+      fireEvent.change(screen.getByLabelText("✉️ Email"), {
+        target: {
+          value: email,
+        },
+      });
+
+      fireEvent.change(screen.getByLabelText("🔑 Password"), {
+        target: {
+          value: password,
+        },
+      });
+
+      fireEvent.click(screen.getByText("Sign up"));
+
+      await waitFor(() =>
+        expect(fetch).toHaveBeenCalledWith(
+          "/api/authenticate",
+          expect.objectContaining({
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              name,
+              email,
+              password,
+              isSignUp: true,
+            }),
+          })
+        )
+      );
+    });
+
+    it("should handle errors on sign up", async () => {
+      const error = "There was an error";
+
+      fetch.mockImplementationOnce(() =>
+        Promise.resolve({
+          ok: false,
+          text: () => Promise.resolve(error),
+        })
+      );
+
+      render(<LoginPage />);
+
+      const name = "name";
+      const email = "email@email.com";
+      const password = "password";
+
+      fireEvent.click(screen.getByText("Sign up"));
+
+      await waitFor(() => screen.getByLabelText("👩‍💻 Name"));
+
+      fireEvent.change(screen.getByLabelText("👩‍💻 Name"), {
+        target: {
+          value: name,
+        },
+      });
+
+      fireEvent.change(screen.getByLabelText("✉️ Email"), {
+        target: {
+          value: email,
+        },
+      });
+
+      fireEvent.change(screen.getByLabelText("🔑 Password"), {
+        target: {
+          value: password,
+        },
+      });
+
+      fireEvent.click(screen.getByText("Sign up"));
+
+      await waitFor(() => expect(() => screen.getByText(error)).not.toThrow());
+    });
+  });
+});
